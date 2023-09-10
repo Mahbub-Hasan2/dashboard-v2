f1fb22f6a7e3d20a0d55f2450911a15c79538bde
Mahbub-Hasan2 <mahbubdev1@gmail.com>
2023-09-10 20:17:35 +0600

    up

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -4,8 +4,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        // baseUrl: 'https://mahbub-backend.vercel.app',
-        baseUrl: 'http://localhost:5000',
+        baseUrl: 'https://mahbub-backend.vercel.app',
+        // baseUrl: 'http://localhost:5000',
     }),
     tagTypes: ["projects"],
     endpoints: (builder) => ({
