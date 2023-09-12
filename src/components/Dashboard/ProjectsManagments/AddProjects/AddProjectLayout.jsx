b77a0cb51b7741ca334eefc02868e9fdeddc8987
Mahbub-Hasan2 <mahbubdev1@gmail.com>
2023-09-12 11:20:37 +0600

    up

--- src/components/Dashboard/ProjectsManagments/AddProjects/AddProjectLayout.jsx
+++ src/components/Dashboard/ProjectsManagments/AddProjects/AddProjectLayout.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import AddProjectSidebar from './AddProjectSidebar';
+import { BiRefresh } from 'react-icons/bi';
+import { useDispatch } from 'react-redux';
+import { resetProjectData } from '../../../../features/project/projectSlice';
 
 const AddProjectLayout = () => {
+    const dispatch = useDispatch();
+
+    const handleResetProjectData = () => {
+        dispatch(resetProjectData());
+    };
     return (
         <div className='py-10'>
             <div className="container mx-auto md:px-28">
@@ -10,8 +18,13 @@ const AddProjectLayout = () => {
                     <div className="col-span-3">
                         <AddProjectSidebar />
                     </div>
-                    <div className="col-span-7 border rounded-xl p-8">
+                    <div className="col-span-7 border rounded-xl p-8 relative">
                         <Outlet />
+                        <div className="absolute top-10 -right-3">
+                            <button onClick={handleResetProjectData}>
+                                <BiRefresh className='text-2xl hover:text-primary' />
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
